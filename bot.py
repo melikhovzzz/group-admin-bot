9404b8df727b986944a0fc3733c8218afc310c40
Gregory Melikhov <gr.melikhov12@gmail.com>
2025-11-16 18:51:16 +0200

    Translate bot.py comments and messages to English
    Updated bot responses and comments to English.

--- bot.py
+++ bot.py
@@ -1,39 +1,36 @@
-import telebot # библиотека telebot
-from TranslatorBot.config import token # импорт токена
+import telebot # telebot library
+from TranslatorBot.config import token # token import, take it from @BotFather
 
 bot = telebot.TeleBot(token) 
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.reply_to(message, "Привет! Я бот для управления чатом.")
+    bot.reply_to(message, "Hello! I am a chat manager bot.")
 
 @bot.message_handler(commands=['ban'])
 def ban_user(message):
-    if message.reply_to_message: #проверка на то, что эта команда была вызвана в ответ на сообщение 
-        chat_id = message.chat.id # сохранение id чата
-         # сохранение id и статуса пользователя, отправившего сообщение
-        user_id = message.reply_to_message.from_user.id
-        user_status = bot.get_chat_member(chat_id, user_id).status 
-         # проверка пользователя
+    if message.reply_to_message: # checking that this command was called in response to a message 
+        chat_id = message.chat.id # saving chat id
+        user_id = message.reply_to_message.from_user.id # saving user id
+        # user check
         if user_status == 'administrator' or user_status == 'creator':
-            bot.reply_to(message, "Невозможно забанить администратора.")
+            bot.reply_to(message, "Unable to ban administrator.")
         else:
-            bot.ban_chat_member(chat_id, user_id) # пользователь с user_id будет забанен в чате с chat_id
-            bot.reply_to(message, f"Пользователь @{message.reply_to_message.from_user.username} был забанен.")
+            bot.ban_chat_member(chat_id, user_id) # the user with user_id will be banned from the chat with chat_id
+            bot.reply_to(message, f"User @{message.reply_to_message.from_user.username} was banned.")
     else:
-        bot.reply_to(message, "Эта команда должна быть использована в ответ на сообщение пользователя, которого вы хотите забанить.")
+        bot.reply_to(message, "This command should be used in response to a message from a user you want to ban.")
 
 @bot.message_handler(commands=['unban'])
 def unban_user(message):
-    if message.reply_to_message: #проверка на то, что эта команда была вызвана в ответ на сообщение 
-        chat_id = message.chat.id # сохранение id чата
-         # сохранение id и статуса пользователя, отправившего сообщение
-        user_id = message.reply_to_message.from_user.id
+    if message.reply_to_message: # checking that this command was called in response to a message 
+        chat_id = message.chat.id # saving chat id
+        user_id = message.reply_to_message.from_user.id # saving user id
         #user_status = bot.get_chat_member(chat_id, user_id).status 
-        bot.unban_chat_member(chat_id, user_id) # пользователь с user_id будет разбанен в чате с chat_id
-        bot.reply_to(message, f"Пользователь @{message.reply_to_message.from_user.username} был разбанен.")
+        bot.unban_chat_member(chat_id, user_id) # the user with user_id will be unbanned from the chat with chat_id
+        bot.reply_to(message, f"User @{message.reply_to_message.from_user.username} was unbanned.")
     else:
-        bot.reply_to(message, "Эта команда должна быть использована в ответ на сообщение пользователя, которого вы хотите разбанить.")
+        bot.reply_to(message, "This command should be used in response to a message from a user you want to ban.")
 
 @bot.message_handler(content_types=['new_chat_members'])
 def new_member(message):
